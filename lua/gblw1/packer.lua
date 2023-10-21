6a3c68640499dd52faf47a1c7d0249a33c297b99
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2023-10-21 01:38:43 -0300

    add: lualine (status bar)

--- lua/gblw1/packer.lua
+++ lua/gblw1/packer.lua
@@ -63,4 +63,9 @@ return require('packer').startup(function(use)
     end
   }
 
+  use 'nvim-tree/nvim-web-devicons' -- OPTIONAL: for file icons
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
 end)

bfc03f37c953e0faa6ba7a0976612e39c87d35ef
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2023-10-23 13:49:26 -0300

    feat: auto formatting

---
+++ lua/gblw1/formatting.lua
@@ -0,0 +1,34 @@
+return {
+	"stevearc/conform.nvim",
+	evento = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				html = { "prettier" },
+				css = { "prettier" },
+				json = { "prettier" },
+				lua = { "stylua" },
+				go = { "gofumpt", "goimports", "goimports-reviser", "golines" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end, { desc = "Format file" })
+	end,
+}

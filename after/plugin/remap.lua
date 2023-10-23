f5e31ba215a69082fe76db0cff45682668665ad5
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2023-10-23 00:20:12 -0300

    fix: description message format with lsp

--- after/plugin/remap.lua
+++ after/plugin/remap.lua
@@ -26,7 +26,7 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>t", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = '[F]ormat buffer' })
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = '[F]ormat current buffer with LSP' })
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")

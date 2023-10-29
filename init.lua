198a7b7729be212efefe8a18ba5925b0ec689be1
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2023-10-29 00:22:32 -0300

    feat: treesitter-context

--- init.lua
+++ init.lua
@@ -227,6 +227,8 @@ require("lazy").setup({
 		build = ":TSUpdate",
 	},
 
+	"nvim-treesitter/nvim-treesitter-context",
+
 	-- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
 	--       These are some example plugins that I've included in the kickstart repository.
 	--       Uncomment any of the lines below to enable them.

762af8bfc8c8d9d615f0d525e658b5b3cbe98842
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2024-04-19 11:00:45 -0300

    feat: getting rid of the "which-key" plugin + adding "screen-key" plugin

--- init.lua
+++ init.lua
@@ -1,42 +1,3 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-
-Kickstart.nvim is *not* a distribution.
-
-Kickstart.nvim is a template for your own configuration.
-  The goal is that you can read every line of code, top-to-bottom, understand
-  what your configuration is doing, and modify it to suit your needs.
-
-  Once you've done that, you should start exploring, configuring and tinkering to
-  explore Neovim!
-
-  If you don't know anything about Lua, I recommend taking some time to read through
-  a guide. One possible example:
-  - https://learnxinyminutes.com/docs/lua/
-
-
-  And then you can explore or search through `:help lua-guide`
-  - https://neovim.io/doc/user/lua-guide.html
-
-
-Kickstart Guide:
-
-I have left several `:help X` comments throughout the init.lua
-You should run that command and read that help section for more information.
-
-In addition, I have some `NOTE:` items throughout the file.
-These are for you, the reader to help understand what is happening. Feel free to delete
-them once you know what you're doing, but they should serve as a guide for when you
-are first encountering a few different constructs in your nvim config.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now :)
---]]
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
@@ -74,6 +35,14 @@ require("lazy").setup({
 	"rbong/vim-flog",
 	"tpope/vim-fugitive",
 	"tpope/vim-rhubarb",
+
+	-- ScreenKey for nvim
+	{
+		"NStefan002/screenkey.nvim",
+		cmd = "Screenkey",
+		version = "*",
+		config = true,
+	},
 
 	-- Detect tabstop and shiftwidth automatically
 	-- "tpope/vim-sleuth",
@@ -115,7 +84,7 @@ require("lazy").setup({
 	},
 
 	-- Useful plugin to show you pending keybinds.
-	{ "folke/which-key.nvim", opts = {} },
+	-- { "folke/which-key.nvim", opts = {} }, -- i dont need it yk
 	{
 		-- Adds git related signs to the gutter, as well as utilities for managing changes
 		"lewis6991/gitsigns.nvim",
@@ -515,15 +484,16 @@ local on_attach = function(_, bufnr)
 end
 
 -- document existing key chains
-require("which-key").register({
-	["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-	["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-	["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
-	["<leader>gd"] = { name = "[G]it [D]iff", _ = "which_key_ignore" },
-	["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-	["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-	["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-})
+-- i dont need it yk
+-- require("which-key").register({
+-- 	["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+-- 	["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+-- 	["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
+-- 	["<leader>gd"] = { name = "[G]it [D]iff", _ = "which_key_ignore" },
+-- 	["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+-- 	["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+-- 	["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+-- })
 
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
